886d2e83693134d0452be184d7e3bb547043a20d
LeotendoDev <leotendosteinhoff@gmail.com>
2022-04-23 13:30:11 +0200

    Setup cmake build system

---
+++ bin/CMakeFiles/tokoin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tokoin.dir/src/main.cpp.o"
+  "CMakeFiles/tokoin.dir/src/main.cpp.o.d"
+  "tokoin"
+  "tokoin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tokoin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
 // SPDX-License-Identifier: MIT
+/*
+ * Copyright (C) 2022  Leonard Steinhoff. All rights reserved.
+ * Licensed under MIT
+ *
+ *
+ * You should find a copy of the license in 'LICENSE', if you don't
+ * you can get it on: 
+ *
+ * <https://www.opensource.org/licenses/mit>
+ *
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
